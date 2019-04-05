85355d2a92c8c147d938a2bfb7ea2a2574c5b995
Nizamtmd <nijamtmd@gmail.com>
2019-04-05 10:07:26 +0530

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import {Employee} from './Employee'
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
- name:string="Wipro";
+export class AppComponent {
+
+  name: string = "Wipro";
+  chk: boolean = true;
+
+  ChangeName(val: string): void {
+    this.name = val;
+  }
+
+  toggleCheck() {
+    this.chk = !this.chk;
+  }
+
+
+  emp: Employee[] = [
+    {
+      name: "Nizam"
+
+    },
+    {
+      name: "Dhiren"
+    },
+    {
+      name: "Devi"
+
+    }
+
+  ]
+
 }
